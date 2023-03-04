eb46c2353cf75d645582a31aed41f591da6b6930
Avanti Jaiswal <121394112+AvantiJaiswal@users.noreply.github.com>
2023-03-04 19:23:13 +0530

    Create sample Java list

---
+++ Java List.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        ArrayList<Integer> L = new ArrayList<Integer>(N);
+        for(int i=0;i<N;i++)
+        {
+          int num = scan.nextInt();
+          L.add(num); 
+        }
+        int Q = scan.nextInt();
+        for(int i=0;i<Q;i++)
+        {
+          scan.nextLine();
+          String type = scan.nextLine();
+          if(type.equals("Insert"))
+          {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            L.add(x,y);
+          }else {
+            int x = scan.nextInt();
+            L.remove(x);
+          }
+        }
+        String formattedString = L.toString()
+         .replace(",","")
+         .replace("[","")
+         .replace("]","");
+        System.out.println(formattedString);  
+    }
+}
